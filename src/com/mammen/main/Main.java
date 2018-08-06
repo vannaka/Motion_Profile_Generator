db396c9b619b987798de7ffe7119bed3c066a9c7
Blake Mammen <blakemammen@centurytel.net>
2018-08-06 18:18:44 -0500

    Fix window size issues maybe?

--- src/com/mammen/main/Main.java
+++ src/com/mammen/main/Main.java
@@ -18,17 +18,19 @@ public class Main extends Application
 		try
 		{
 			Pane root = FXMLLoader.load(getClass().getResource("/com/mammen/ui/javafx/MainUI.fxml"));
-			Scene scene = new Scene(root);
-			Dimension res = Toolkit.getDefaultToolkit().getScreenSize();
-	        int width = res.width - 269;
-	        int height = res.height - 111;
+			// Scene scene = new Scene(root);
+			// Dimension res = Toolkit.getDefaultToolkit().getScreenSize();
+	        // int width = res.width - 269;
+	        // int height = res.height - 111;
+	        root.autosize();
 	        
-			primaryStage.setScene(scene);
+			primaryStage.setScene(new Scene(root));
+			primaryStage.sizeToScene();
 			primaryStage.setTitle("Motion Profile Generator");
 			
-			primaryStage.setWidth(width);
-	        primaryStage.setHeight(height);
-	        primaryStage.centerOnScreen();
+			// primaryStage.setWidth(width);
+	        // primaryStage.setHeight(height);
+	        // primaryStage.centerOnScreen();
 
 	        primaryStage.setResizable(false);
 			primaryStage.show();
