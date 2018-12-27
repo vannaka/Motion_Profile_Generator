5b1188d203e101666e489ed392ac42f8d4cd26ec
Luke Mammen <lmammen96@gmail.com>
2018-12-27 16:29:47 -0600

    Fix bug preventing jar from exicuting

--- src/java/com/mammen/main/Main.java
+++ src/java/com/mammen/main/Main.java
@@ -1,41 +1,9 @@
 package com.mammen.main;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.fxml.FXMLLoader;
-
-
-public class Main extends Application 
+public class Main
 {
-	@Override
-	public void start( Stage primaryStage )
-	{
-		try
-		{
-			Pane root = FXMLLoader.load( getClass().getResource("/com/mammen/ui/javafx/main/MainUI.fxml") );
-	        root.autosize();
-
-	        FXMLLoader loader = new FXMLLoader( getClass().getResource("/com/mammen/ui/javafx/main/graphs/PosGraphController.fxml") );
-
-			primaryStage.setScene( new Scene( root ) );
-			primaryStage.sizeToScene();
-			primaryStage.setTitle("Motion Profile generator");
-			primaryStage.setMinWidth( 1280 ); //1170
-	        primaryStage.setMinHeight( 720 ); //790
-	        primaryStage.setResizable( true );
-
-			primaryStage.show();
-		} 
-		catch(Exception e) 
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) 
+	public static void main( String[] args )
 	{	
-		launch(args);
+		MainApp.main( args );
 	}
 }
---
+++ src/java/com/mammen/main/MainApp.java
@@ -0,0 +1,40 @@
+package com.mammen.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application
+{
+    @Override
+    public void start( Stage primaryStage )
+    {
+        try
+        {
+            Pane root = FXMLLoader.load( getClass().getResource("/com/mammen/ui/javafx/main/MainUI.fxml") );
+            root.autosize();
+
+            FXMLLoader loader = new FXMLLoader( getClass().getResource("/com/mammen/ui/javafx/main/graphs/PosGraphController.fxml") );
+
+            primaryStage.setScene( new Scene( root ) );
+            primaryStage.sizeToScene();
+            primaryStage.setTitle("Motion Profile generator");
+            primaryStage.setMinWidth( 1280 ); //1170
+            primaryStage.setMinHeight( 720 ); //790
+            primaryStage.setResizable( true );
+
+            primaryStage.show();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
